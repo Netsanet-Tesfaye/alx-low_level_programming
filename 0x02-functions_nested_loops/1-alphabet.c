6160588d8ed61e592628d9bd4ed4d7d7688caf16
Netsanet-Tesfaye <netsanetkassa55@gmail.com>
2023-04-25 12:12:57 -0700

    the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
-
 /**
- * print_alphabet - prints the alpha in lowercase
+ * print_alphabet - print the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
-	char la;
+	int i;
 
-	for (la = 'a'; la <= 'z'; la++)
-			_putchar(la);
-
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
 	_putchar('\n');
-	return (0);
 }
