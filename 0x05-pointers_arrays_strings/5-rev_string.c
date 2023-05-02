c549bd10c5279ec47dd51d3f98bf91f89970cdea
Netsanet-Tesfaye <netsanetkassa55@gmail.com>
2023-05-02 08:14:46 -0700

    thinks in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * rev_string - Reverses a string
- * @s: Input string
+ * rev_string - reverses a string
+ * @s: Input
  * Return: String in reverse
  */
 void rev_string(char *s)
 {
-	char rev = s[0];
-	int counter = 0;
+	char rv = s[0];
+	int c = 0;
 	int i;
 
-	while (s[counter] != '\0')
-	counter++;
-	for (i = 0; i < counter; i++)
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
 	{
-		counter--;
-		rev = s[i];
-		s[i] = s[counter];
-		s[countr] = rev;
+		c--;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
 	}
 }
