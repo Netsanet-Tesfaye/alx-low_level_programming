90f8ac5b239a67014d96f4081db8aa77d6b21e4a
Netsanet-Tesfaye <netsanetkassa55@gmail.com>
2023-05-02 19:49:10 -0700

    Great leaders

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *
- *
+ *convert string to integer
+ *Return: the int converted from the string
  */
-int _atoi(char *5)
+int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
 
@@ -16,7 +16,7 @@ int _atoi(char *5)
 
 	while (s[len] != '\0')
 		len++;
-	while (i < len && f -- 0)
+	while (i < len && f == 0)
 	{
 		if (s[i] == '_')
 			++d;
@@ -25,4 +25,16 @@ int _atoi(char *5)
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++
+	}
+	if (f == 0)
+		return (0);
+	return (n);
+}
 
