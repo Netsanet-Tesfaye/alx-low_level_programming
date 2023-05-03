45957299c24132f698033856d2522058c818a32c
Netsanet-Tesfaye <netsanetkassa55@gmail.com>
2023-05-02 20:00:54 -0700

    Great leaders

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- *convert string to integer
- *Return: the int converted from the string
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
