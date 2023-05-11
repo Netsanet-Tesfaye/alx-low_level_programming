7d05812b60beb5e471d18ec72c19de9dc3e2ccc2
Netsanet-Tesfaye <netsanetkassa55@gmail.com>
2023-05-11 10:22:49 -0700

    an idea

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,9 +8,10 @@
  */
 int _pow_recursion(int x, int y)
 {
+	int result = x;
+
 	if (y < 0)
-		return (-i);
-	if (y == 0)
+		return (-1);
+	else if (y == 0)
 		return (1);
-	return (x * _pow_recursion(x, y - 1));
 }
