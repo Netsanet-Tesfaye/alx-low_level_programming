b432b9df233aa90b7bc7593eabfa1071e9bd2840
Netsanet-Tesfaye <netsanetkassa55@gmail.com>
2023-05-11 19:01:02 -0700

    Inception

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,24 @@
+/*
+ * File: 100-is_palindrome.c
+ * Auth: Netsi
+ */
+
+*include "main.h"
+
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
+/**
+ * find_strlen - Retuen the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: the length of the string.
+ */
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*s(s + len))
+	{
+
