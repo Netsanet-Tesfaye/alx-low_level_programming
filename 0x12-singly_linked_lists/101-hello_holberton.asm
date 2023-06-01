5540ae3c11dec5fd322d3e29744730a85e8d29fd
Netsanet-Tesfaye <netsanetkassa55@gmail.com>
2023-06-01 12:53:26 -0700

    Real programmers can write assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,8 +2,8 @@
            extern   printf
 main:
         mov  edi, format
-        xor  eax eax
+        xor  eax, eax
         call printf
         mov  eax, 0
         ret
-format: db Hello, Holberton\n',0
+format: db 'Hello, Holberton\n',0
