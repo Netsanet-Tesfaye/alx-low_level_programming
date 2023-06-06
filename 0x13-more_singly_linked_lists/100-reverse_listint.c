277c7438829919abdf9e52b1583b74e5614abc0c
Netsanet-Tesfaye <netsanetkassa55@gmail.com>
2023-06-06 13:15:47 -0700

    List length

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,7 @@
+#include "lists.h"
+/**
+ * revers_listint - reverses a listint_tlist.
+ * @head: a pointer to the address of the head of the list_t list.
+ *
+ * Return: a pointer to the first node of the reversed list.
+ */
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * insert -nodeint_at_index - inserts a new node to a listint_t list.
+ * @head: a pointer to the address of the head of the listint_t list.
+ * @idx: the index of the listint_t list where the new node should be added - indexes start at - 0.
+ * @n: the integer
+ *
+ * Return: if the function fails - NULL.
+ * otherwise - the address of the new node.
+ */
