70fe6b6702a8d7298890d31eeb2b2f581fcca7a1
Netsanet-Tesfaye <netsanetkassa55@gmail.com>
2023-06-12 13:31:32 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int h, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,12 +20,12 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o == -1 || w == -1);
+	h = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(h, text_content, len);
 
-	if (0 == -1 || w == -1)
+	if (h == -1 || w == -1)
 		return (-1);
-	close(0);
+	close(h);
 
 	return (1);
 }
