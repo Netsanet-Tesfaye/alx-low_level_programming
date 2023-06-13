29d172dd4e28ca79653810ef708801cc4af0a139
Netsanet-Tesfaye <netsanetkassa55@gmail.com>
2023-06-12 20:00:03 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -69,7 +69,7 @@ int main(int argc, char *argv[])
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREATE | O_WRONLY | O_TRUNC, 0600);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	do {
 		if (from == -1 || r == -1)
