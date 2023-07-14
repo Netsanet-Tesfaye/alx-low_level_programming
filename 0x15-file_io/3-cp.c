ba4eb9be4a28eb7fb15431a8657cb2e7abae82b5
Netsanet-Tesfaye <netsanetkassa55@gmail.com>
2023-07-14 13:52:32 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -87,7 +87,7 @@ int main(int argc, char *argv[])
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
-			dprintf(SRDERR_FILENO,
+			dprintf(STDERR_FILENO,
 					"Error: can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
